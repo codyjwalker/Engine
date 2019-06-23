4f2430d2e26e1b2fe181db6cc105e5c27dd82fa7
Cody Walker <themightycoder@gmail.com>
2019-06-23 12:52:15 -0700

    We got ourselves a fuckin camera bissshhhhhh

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -3,6 +3,7 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 
+import entities.Camera;
 import entities.Entity;
 import models.RawModel;
 import models.TexturedModel;
@@ -25,9 +26,10 @@ public class MainGameLoop {
 		DisplayManager.createDisplay();
 		// Create Loader, Renderer, & Shader so that we can use them.
 		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
 		StaticShader shader = new StaticShader();
+		Renderer renderer = new Renderer(shader);
 
+		/*
 		// Vertices of a simple rectangle.
 		float[] vertices = { -0.5f, 0.5f, 0f, // v0
 				-0.5f, -0.5f, 0f, // v1
@@ -47,19 +49,109 @@ public class MainGameLoop {
 				1,1,	// v2
 				1,0		// v3
 		};
+		*/
+		
+
+
+		float[] vertices = {			
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,0.5f,-0.5f,		
+				
+				-0.5f,0.5f,0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				0.5f,0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				-0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,0.5f,
+				-0.5f,0.5f,-0.5f,
+				0.5f,0.5f,-0.5f,
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,-0.5f,0.5f,
+				-0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,0.5f
+				
+		};
+		
+		float[] textureCoordinates = {
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0
+
+				
+		};
+		
+		int[] indices = {
+				0,1,3,	
+				3,1,2,	
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,	
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
+
+		};
+
+
 
 		RawModel model = loader.loadToVAO(vertices, textureCoordinates, indices);
 		ModelTexture texture = new ModelTexture(loader.loadTexture("sassy"));
 		TexturedModel texturedModel = new TexturedModel(model, texture);
-		Entity entity = new Entity(texturedModel, new Vector3f(-1, 0, 0), 0, 0, 0, 1);
+		Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
+		Camera camera = new Camera();
 
 		// The actual game loop. Exit when user clicks 'x' button.
 		while (!Display.isCloseRequested()) {
+			entity.increaseRotation(1, 1, 0);
+			camera.move();
 			// Prepare the Renderer each frame.
 			renderer.prepare();
 
 			// Start the shader program before rendering.
 			shader.start();
+			
+			// Load camera into shader.
+			shader.loadViewMatrix(camera);
 
 			// Render the model each frame.
 			renderer.render(entity, shader);
--- src/renderEngine/Renderer.java
+++ src/renderEngine/Renderer.java
@@ -1,5 +1,6 @@
 package renderEngine;
 
+import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL20;
@@ -17,12 +18,28 @@ import toolbox.Maths;
  * Purpose:	Renders the model from the VAO.
  */
 public class Renderer {
+	
+	private static final float FOV = 70;
+	private static final float NEAR_PLANE = 0.1f;
+	private static final float FAR_PLANE = 1000;
+	
+	private Matrix4f projectionMatrix;
+	
+	public Renderer(StaticShader shader) {
+		createProjectionMatrix();
+		shader.start();
+		shader.loadProjectionMatrix(projectionMatrix);
+		shader.stop();
+	}
 
 	// Called once every frame to prepare OpenGL to render the game.
 	public void prepare() {
+		// Tell OpenGL to test which triangles are in front of each other.
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
 		// Clear color from the last frame.
-		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-		GL11.glClearColor(1, 0, 0, 1);
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+		// Set color of background.
+		GL11.glClearColor(0, 0.3f, 0.1f, 1);
 	}
 
 	// Renders the raw model.
@@ -47,6 +64,22 @@ public class Renderer {
 		GL20.glDisableVertexAttribArray(0);
 		GL20.glDisableVertexAttribArray(1);
 		GL30.glBindVertexArray(0);
+	}
+	
+	// Creates the projection matrix.
+	private void createProjectionMatrix() {
+		float aspectRatio = (float) Display.getWidth() / (float) Display.getHeight();
+		float yScale = (float) ((1f / Math.tan(Math.toRadians(FOV / 2f))) * aspectRatio);
+		float xScale = yScale / aspectRatio;
+		float frustumLength = FAR_PLANE - NEAR_PLANE;
+		
+		projectionMatrix = new Matrix4f();
+		projectionMatrix.m00 = xScale;
+		projectionMatrix.m11 = yScale;
+		projectionMatrix.m22 = - ((FAR_PLANE + NEAR_PLANE) / frustumLength);
+		projectionMatrix.m23 = -1;
+		projectionMatrix.m32 = - ((2 * NEAR_PLANE * FAR_PLANE) / frustumLength);
+		projectionMatrix.m33 = 0;
 	}
 
 }
