4f352b98620332b25c347840de4a21336a974fd1
Cody Walker <themightycoder@gmail.com>
2019-06-28 16:13:45 -0700

    Cleaned up main() by making init and terminate methods

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -12,8 +12,6 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.MasterRenderer;
 import renderEngine.OBJLoader;
-import renderEngine.Renderer;
-import shaders.StaticShader;
 import textures.ModelTexture;
 
 /*
@@ -23,41 +21,28 @@ import textures.ModelTexture;
  */
 public class MainGameLoop {
 
+	private static Loader loader;
+	private static RawModel rawModel;
+	private static ModelTexture texture, modelTexture;
+	private static TexturedModel texturedModel;
+	private static Entity entity;
+	private static Light light;
+	private static Camera camera;
+	private static MasterRenderer renderer;
+
 	public static void main(String[] args) {
-
-		// Open up the display.
-		DisplayManager.createDisplay();
-
-		// Create Loader.
-		Loader loader = new Loader();
-
-		// Load up RawModel & ModelTexture, make texturedModel, & extract its texture.
-		RawModel rawModel = OBJLoader.loadObjModel("DragonBlender", loader);
-		ModelTexture texture = new ModelTexture(loader.loadTexture("white"));
-		TexturedModel texturedModel = new TexturedModel(rawModel, texture);
-		ModelTexture modelTexture = texturedModel.getTexture();
-		// Set damper and reflectivity values.
-		modelTexture.setShineDamper(10);
-		modelTexture.setReflectivity(1);
-		// Make Entity with TexturedModel.
-		Entity entity = new Entity(texturedModel, new Vector3f(0, -4, -25), 0, 0, 0, 1);
-		// Make lightsource.
-		Light light = new Light(new Vector3f(0, 0, -20), new Vector3f(1, 1, 1));
-		// Create camera.
-		Camera camera = new Camera();
-
-		// Create the MasterRenderer
-		MasterRenderer renderer = new MasterRenderer();
-
+		init();
+		
 		// The actual game loop. Exit when user clicks 'x' button.
 		while (!Display.isCloseRequested()) {
 			entity.increaseRotation(0, 0.3f, 0);
 			camera.move();
 
-			/*
-			 * // For each entity, for each frame, process the entity. for (Entity entity :
-			 * entities) { renderer.processEntity(entity); }
-			 */
+			// For each entity, for each frame, process the entity. 
+			//for (Entity entity : entities) {
+			//	renderer.processEntity(entity);
+			//}
+
 			renderer.processEntity(entity);
 
 			// Render each frame.
@@ -66,12 +51,46 @@ public class MainGameLoop {
 			// Update the display each frame.
 			DisplayManager.updateDisplay();
 		}
+		terminate();
+	}
 
+	// Initializes all objects needed for rendering.
+	private static void init() {
+		// Open up the display.
+		DisplayManager.createDisplay();
+
+		// Create Loader.
+		loader = new Loader();
+
+		// Load up RawModel & ModelTexture, make texturedModel, & extract its texture.
+		rawModel = OBJLoader.loadObjModel("DragonBlender", loader);
+		texture = new ModelTexture(loader.loadTexture("white"));
+		texturedModel = new TexturedModel(rawModel, texture);
+		modelTexture = texturedModel.getTexture();
+
+		// Set damper and reflectivity values.
+		modelTexture.setShineDamper(10);
+		modelTexture.setReflectivity(1);
+
+		// Make Entity with TexturedModel.
+		entity = new Entity(texturedModel, new Vector3f(0, -4, -25), 0, 0, 0, 1);
+
+		// Make lightsource.
+		light = new Light(new Vector3f(0, 0, -20), new Vector3f(1, 1, 1));
+
+		// Create camera.
+		camera = new Camera();
+
+		// Create the MasterRenderer
+		renderer = new MasterRenderer();
+	}
+	
+	// Cleans up renderer & loader, then closes display.
+	private static void terminate() {
 		// Cleanup loader & renderer upon closing.
 		renderer.cleanUp();
 		loader.cleanUp();
 		// Close display once loop is exited.
 		DisplayManager.closeDisplay();
 	}
-
 }
