c159d1dc0fcbeb1ec3facf781cffd0e490520d32
Cody Walker <themightycoder@gmail.com>
2019-06-28 23:11:43 -0700

    Added a bunch of code for my own textures to be rendered in main

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -38,6 +38,7 @@ public class MainGameLoop {
 	private static Terrain terrain, terrain2;
 	private static TexturedModel grass;
 	private static TexturedModel fern;
+	private static TexturedModel bush0, bush1, bush2, rock0, rock1, tree0, tree1, tree2, bench0;
 
 	// Initializes all objects needed for rendering.
 	private static void init() {
@@ -64,30 +65,67 @@ public class MainGameLoop {
 				new ModelTexture(loader.loadTexture("grassTexture")));
 		grass.getTexture().setTransparancy(true);
 		grass.getTexture().setUseFakeLighting(true);
+
 		fern = new TexturedModel(OBJLoader.loadObjModel("fern", loader), new ModelTexture(loader.loadTexture("fern")));
 		fern.getTexture().setTransparancy(true);
 
+		bush0 = new TexturedModel(OBJLoader.loadObjModel("simpleBush0", loader),
+				new ModelTexture(loader.loadTexture("tree")));
+		bush1 = new TexturedModel(OBJLoader.loadObjModel("simpleBush1", loader),
+				new ModelTexture(loader.loadTexture("tree")));
+		bush2 = new TexturedModel(OBJLoader.loadObjModel("simpleBush2", loader),
+				new ModelTexture(loader.loadTexture("tree")));
+//		rock0 = new TexturedModel(OBJLoader.loadObjModel("simpleRock0", loader),
+//				new ModelTexture(loader.loadTexture("tree")));
+//		rock1 = new TexturedModel(OBJLoader.loadObjModel("simpleRock1", loader),
+//				new ModelTexture(loader.loadTexture("tree")));
+		tree0 = new TexturedModel(OBJLoader.loadObjModel("simpleTree0", loader),
+				new ModelTexture(loader.loadTexture("tree")));
+//		tree1 = new TexturedModel(OBJLoader.loadObjModel("simpleTree1", loader),
+//				new ModelTexture(loader.loadTexture("tree")));
+//		tree2 = new TexturedModel(OBJLoader.loadObjModel("simpleTree2", loader),
+//				new ModelTexture(loader.loadTexture("tree")));
+
 		// Create Entity with TexturedModel.
 		entities = new ArrayList<Entity>();
-		entities.add(new Entity(dragon, new Vector3f(0, -4, -25), 0, 0, 0, 1));
 		Random random = new Random();
 		for (int i = 0; i < 500; i++) {
-			entities.add(new Entity(grass, new Vector3f(random.nextFloat() * 800 - 400, 0, random.nextFloat() * -600),
-					0, 0, 0, 3));
-			entities.add(new Entity(fern, new Vector3f(random.nextFloat() * 800 - 400, 0, random.nextFloat() * -600), 0,
-					0, 0, 2));
+//			entities.add(new Entity(grass, new Vector3f(random.nextFloat() * 800 - 400, 0, random.nextFloat() * -600),
+//					0, 0, 0, 3));
+//			entities.add(new Entity(fern, new Vector3f(random.nextFloat() * 800 - 400, 0, random.nextFloat() * -600), 0,
+//					0, 0, 2));
+
+			entities.add(new Entity(bush0, new Vector3f(random.nextFloat() * 800 - 400, 0, random.nextFloat() * -600),
+					0, 0, 0, 2));
+			entities.add(new Entity(bush1, new Vector3f(random.nextFloat() * 800 - 400, 0, random.nextFloat() * -600),
+					0, 0, 0, 2));
+			entities.add(new Entity(bush2, new Vector3f(random.nextFloat() * 800 - 400, 0, random.nextFloat() * -600),
+					0, 0, 0, 2));
+//			entities.add(new Entity(rock0, new Vector3f(random.nextFloat() * 800 - 400, 0, random.nextFloat() * -600),
+//					0, 0, 0, 2));
+//			entities.add(new Entity(rock1, new Vector3f(random.nextFloat() * 800 - 400, 0, random.nextFloat() * -600),
+//					0, 0, 0, 2));
+			entities.add(new Entity(tree0, new Vector3f(random.nextFloat() * 800 - 400, -3.5f, random.nextFloat() * -600),
+					0, 0, 0, 18));
+//			entities.add(new Entity(tree1, new Vector3f(random.nextFloat() * 800 - 400, 0, random.nextFloat() * -600),
+//					0, 0, 0, 2));
+//			entities.add(new Entity(tree2, new Vector3f(random.nextFloat() * 800 - 400, 0, random.nextFloat() * -600),
+//					0, 0, 0, 2));
+//			entities.add(new Entity(bench0, new Vector3f(random.nextFloat() * 800 - 400, 0, random.nextFloat() * -600),
+//					0, 0, 0, 2));
+
 		}
 		for (int i = 0; i < 100; i++) {
-			entities.add(new Entity(dragon, new Vector3f(random.nextFloat() * 800 - 400, 0, random.nextFloat() * -600),
-					0, 0, 0, 3));
+			entities.add(new Entity(dragon,
+					new Vector3f(random.nextFloat() * 800 - 400, 100, random.nextFloat() * -600), 0, 0, 0, 3));
 		}
 
 		// Create lightsource.
 		light = new Light(new Vector3f(0, 100, 0), new Vector3f(1, 1, 1));
 
 		// Create terrain.
-		terrain = new Terrain(0, -1, loader, new ModelTexture(loader.loadTexture("desert")));
-		terrain2 = new Terrain(-1, -1, loader, new ModelTexture(loader.loadTexture("desert")));
+		terrain = new Terrain(0, -1, loader, new ModelTexture(loader.loadTexture("grassTerrain")));
+		terrain2 = new Terrain(-1, -1, loader, new ModelTexture(loader.loadTexture("grassTerrain")));
 
 		// Create camera.
 		camera = new Camera();
